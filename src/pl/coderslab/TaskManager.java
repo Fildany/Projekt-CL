eb89b485492bd0d72b095a16e786e64ae6ba1a3f
Fildany <filip.danyluk@gmail.com>
2021-05-21 01:30:43 +0200

    v2

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,14 +1,30 @@
 package pl.coderslab;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
+
 public class TaskManager {
+
     static String[][] tasks;
 
     public static void main(String[] args) {
-
-
+        tasks = loadTasks();
         menu();
+    }
+
+    private static String[][] loadTasks() {
+        Path tasksPath = Paths.get("tasks.csv");
+
+        try {
+            Scanner scanner = new Scanner(tasksPath.toFile());
+        } catch (FileNotFoundException e) {
+            System.out.println(ConsoleColors.RED + "File not found: " + tasksPath.toAbsolutePath());
+        }
+
+        return new String[0][];
     }
 
     public static String menu() {
@@ -22,7 +38,7 @@ public class TaskManager {
         System.out.println("exit");
 
         String option = skaner.nextLine();
-        while(!option.equals("add") && !option.equals("remove") && !option.equals("list") && !option.equals("exit") ){
+        while (!option.equals("add") && !option.equals("remove") && !option.equals("list") && !option.equals("exit")) {
             System.out.println("Zły wybór");
             option = skaner.nextLine();
         }
