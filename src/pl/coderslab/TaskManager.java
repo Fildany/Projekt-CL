acbb504fb089545aeffe00701a98bd097395087a
Fildany <filip.danyluk@gmail.com>
2021-05-21 00:07:00 +0200

    V1

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,9 +1,34 @@
 package pl.coderslab;
 
+import java.util.Scanner;
+
 public class TaskManager {
+    static String[][] tasks;
 
     public static void main(String[] args) {
-        // write your code here
+
+
+        menu();
     }
-    
+
+    public static String menu() {
+
+        Scanner skaner = new Scanner(System.in);
+
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+        System.out.println(ConsoleColors.WHITE + "add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+
+        String option = skaner.nextLine();
+        while(!option.equals("add") && !option.equals("remove") && !option.equals("list") && !option.equals("exit") ){
+            System.out.println("Zły wybór");
+            option = skaner.nextLine();
+        }
+
+        skaner.close();
+        return option;
+    }
+
 }
